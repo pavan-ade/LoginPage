0aa1d8a6a1abf4f168fbc889a42b85297c0c7c92
pavan-ade <pavanade09@gmail.com>
2024-08-04 22:53:40 +0530

    initail push For Login Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'Login';
+  islogin = true;
+  selectedRole = '';
+  userName = '';
+  password = '';
+
+  logout() { 
+    this.islogin = true;
+  }
+
+  login() {
+    this.islogin = false;
+  }
+
+  onRoleChange(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    this.selectedRole = selectElement.value;
+  }
 }
